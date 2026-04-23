158560fe2b66700f35f79c27f7d76c3a0f49e3e4
apro980 <teran980.1@gmail.com>
2026-04-23 17:22:15 -0500

    subiendo ejercicio 1 fallido python

---
+++ python/1_hallas_area_triangulo.py
@@ -0,0 +1,17 @@
+"""
+Algoritmo que calcula a área de um triângulo
+leyendo su base y altura, considerando el tipo de dato
+"""
+# Definir variables, tipo de dato e ingreso de datos
+
+base = 7
+altura = 15
+area = (7*15/)/2
+print (f"el area del triangulo es: {float{area}}m2")
+
+#definir variables, tipo de dato e ingreso de datos
+
+base = int (input {"ingrese la base del triangulo: "})
+altura = int (input {"ingrese la altura del triangulo ":})
+area = (base*altura)/2
+print (f"el area del triangulo cuando su base  es: {float{area}}m2")
---
+++ python/intro.py
@@ -0,0 +1,27 @@
+# Fundamentos basicos de python
+
+# comentario en una sola linea usar "#"
+## comentario multilinea """ """
+"""
+este es un comentario multilinea
+"""
+
+# declarar variables
+"""
+nombre_indentifica valor
+
+edad_persona
+edad = int 
+mensaje = 
+
+"""
+
+# tipo de dato
+edad = 18
+print (edad)
+
+mensaje = "rage your dream"
+print (mensaje)
+
+#presentacion mas avanzada uso f""
+print (f"usted tiene {edad} años y su mensaje es: {mensaje}")
